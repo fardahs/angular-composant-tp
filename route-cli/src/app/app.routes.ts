c3ad4cbcf93f12b5a9bef1ccc8a287f75cca0f3b
Farda HOUMADI <farda.houmadi@hotmail.fr>
2019-03-29 15:48:49 +0100

    Routes hiérarchiques

--- route-cli/src/app/app.routes.ts
+++ route-cli/src/app/app.routes.ts
@@ -4,6 +4,8 @@ import {DetailComponent} from "./detail/detail.component";
 import {PATH_HOME, PATH_DETAIL, PATH_DETAIL_FORMATIONS, PATH_DETAIL_SKILLS} from './app.routes.constantes';
 import {FormationsComponent} from "./formations/formations.component";
 import {ConnaissancesComponent} from "./connaissances/connaissances.component";
+import {LoggedInGuardGuard} from "./guards/logged-in-guard.guard";
+
 
 export const ROUTES:Routes = [
   {path:PATH_HOME, component:HomeComponent},
@@ -13,8 +15,14 @@ export const ROUTES:Routes = [
     children:[
       {path:"", pathMatch:'full', redirectTo:PATH_DETAIL_FORMATIONS},
       {path:PATH_DETAIL_FORMATIONS + '/:idFormation', component:FormationsComponent},
-      {path:PATH_DETAIL_SKILLS + '/:idSkills', component:ConnaissancesComponent}
+      {
+        path:PATH_DETAIL_SKILLS + '/:idSkills',
+        component:ConnaissancesComponent,
+        canActivate: [LoggedInGuardGuard]
+      }
     ]
-  }
+  },
+
+
 
 ];
---
+++ route-cli/src/app/guards/logged-in-guard.guard.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {ActivatedRoute, ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree} from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoggedInGuardGuard implements  CanActivate{
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+      return true;
+  }
+}
+
+
+  
+
